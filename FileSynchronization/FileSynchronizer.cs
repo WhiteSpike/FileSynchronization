295661c3102d03bf6acc6e0149e650e3675f35b6
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2026-09-03 14:23:20 +0100

    Initial implementation of synchronization phase
    - Implemented synchronization from source directory to target directory through usage of FileSystemWatcher that notifies of any changes performed in the source directory such as file creations, copies (& modifications) and deletions.
    - These operations are then stored in the synchronizer untill it hits the synchronization interval which executes all its queued operations accordingly (order of execution is preserved).
    - Implemented string extension to join directory path string to filename string.
    
    Need to revise the File operations as quickly creating a file/directory and renaming it causes it to crash due to previous file not existing in source directory.
    Need to revise if the notifyFilter can have less flags toggled on.
    Need to add an initial synchronization which it copies all the files present in the source directory into the target directory.
    Still need to implement log file.

--- FileSynchronization/FileSynchronizer.cs
+++ FileSynchronization/FileSynchronizer.cs
@@ -3,19 +3,108 @@
     internal class FileSynchronizer
     {
         CommandLineArguments parameters;
-        // TODO Keep track of operations required to synchronize
+        FileSystemWatcher sourceWatcher;
+        LinkedList<FileSystemEventArgs> queueOperations;
 
         public FileSynchronizer(CommandLineArguments parameters)
         {
             this.parameters = parameters;
+            this.queueOperations = new LinkedList<FileSystemEventArgs>();
+            SetupSourceWatcher();
+        }
+
+        void SetupSourceWatcher()
+        {
+            this.sourceWatcher = new(parameters.SourcePath);
+
+            sourceWatcher.NotifyFilter = NotifyFilters.Attributes
+                                 | NotifyFilters.CreationTime
+                                 | NotifyFilters.DirectoryName
+                                 | NotifyFilters.FileName
+                                 | NotifyFilters.LastAccess
+                                 | NotifyFilters.LastWrite
+                                 | NotifyFilters.Security
+                                 | NotifyFilters.Size;
+            sourceWatcher.Created += OnSourceCreated;
+            sourceWatcher.Changed += OnSourceChanged;
+            sourceWatcher.Deleted += OnSourceDeleted;
+            sourceWatcher.Renamed += OnSourceRenamed;
+            sourceWatcher.IncludeSubdirectories = true;
+            sourceWatcher.EnableRaisingEvents = true;
+        }   
+
+        void OnSourceChanged(object sender, FileSystemEventArgs e)
+        {
+            queueOperations.AddFirst(e);
+            Console.WriteLine($"Source file {e.ChangeType}: {e.FullPath}");
+        }
+
+        void OnSourceRenamed(object sender, RenamedEventArgs e)
+        {
+            queueOperations.AddFirst(e);
+            Console.WriteLine($"Source file renamed from {e.OldFullPath} to {e.FullPath}");
+        }
+
+        void OnSourceCreated(object sender, FileSystemEventArgs e)
+        {
+            queueOperations.AddFirst(e);
+            Console.WriteLine($"Source file created: {e.FullPath}");
+        }
+
+        void OnSourceDeleted(object sender, FileSystemEventArgs e)
+        {
+            queueOperations.AddFirst(e);
+            Console.WriteLine($"Source file deleted: {e.FullPath}");
         }
 
         internal void Synchronize()
         {
-            // TODO Synchronize files from source to destination
+            while(queueOperations.Count > 0)
+            {
+                FileSystemEventArgs operation = queueOperations.Last.Value;
+                queueOperations.RemoveLast();
+                switch (operation.ChangeType)
+                {
+                    case WatcherChangeTypes.Created:
+                        CreateFile(operation, parameters.DestinationPath);
+                        break;
+                    case WatcherChangeTypes.Changed:
+                        CopyFile(operation, parameters.DestinationPath);
+                        break;
+                    case WatcherChangeTypes.Deleted:
+                        DeleteFile(operation, parameters.DestinationPath);
+                        break;
+                    case WatcherChangeTypes.Renamed:
+                        RenamedEventArgs renamedOperation = operation as RenamedEventArgs;
+                        if (renamedOperation != null)
+                        {
+                            RenameFile(renamedOperation, parameters.DestinationPath);
+                        }
+                        break;
+                }
+            }
             Console.WriteLine("Synchronized.");
         }
 
+        void CreateFile(FileSystemEventArgs operation, string destinationRootPath)
+        {
+            File.Copy(operation.FullPath, destinationRootPath.File(operation.Name));
+        }
+
+        void CopyFile(FileSystemEventArgs operation, string destinationRootPath)
+        {
+            File.Copy(operation.FullPath, destinationRootPath.File(operation.Name), overwrite: true);
+        }
+
+        void DeleteFile(FileSystemEventArgs operation, string destinationRootPath)
+        {
+            File.Delete(destinationRootPath.File(operation.Name));
+        }
+
+        void RenameFile(RenamedEventArgs operation, string destinationRootPath)
+        {
+            File.Move(destinationRootPath.File(operation.OldName), destinationRootPath.File(operation.Name));
+        }
         internal int GetSynchronizationInterval()
         {
             return parameters.SynchronizationInterval;
---
+++ FileSynchronization/StringExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace FileSynchronization
+{
+    internal static class StringExtensions
+    {
+        public static string File(this string directory, string filename)
+        {
+            return $"{directory}\\{filename}";
+        }
+    }
+}

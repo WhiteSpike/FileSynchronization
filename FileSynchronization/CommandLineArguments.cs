6c3c6df4a61719c802baaebda6ef2b182310e1c0
WhiteSpike <58040140+WhiteSpike@users.noreply.github.com>
2026-09-03 13:17:45 +0100

    Command Line Parser Implementation
    -Implemented ``CommandLineArguments`` which will contain all the arguments provided through command line. At the moment it contains:
     - Source directory path which is the path to the contents we wish to synchronize from;
     - Destination directory path which is the path to the directory whose contents are from the source directory.
     - Synchronization interval (in seconds) which is how often the synchronizer will update the contents of the destination directory according to the changes applied on source directory.
     - Log file path which is the path to the file with logs from the synchronizer.

---
+++ FileSynchronization/CommandLineArguments.cs
@@ -0,0 +1,85 @@
+﻿namespace FileSynchronization
+{
+    internal class CommandLineArguments
+    {
+        const string PATH_ERROR = "";
+
+        const short SOURCE_FOLDER_INDEX = 0;
+        const short TARGET_FOLDER_INDEX = 1;
+        const short SYNCHRONIZATION_INTERVAL_INDEX = 2;
+        const short LOG_FILE_PATH_INDEX = 3;
+
+        internal string SourcePath;
+        internal string DestinationPath;
+        internal int SynchronizationInterval;
+        internal string LogFilePath;
+
+        public CommandLineArguments(string[] args)
+        {
+            if (args.Length < 4)
+            {
+                SourcePath = PATH_ERROR;
+                DestinationPath = PATH_ERROR;
+                SynchronizationInterval = -1;
+                LogFilePath = PATH_ERROR;
+            }
+            else
+            {
+                SourcePath = ValidateSource(args[SOURCE_FOLDER_INDEX]);
+                DestinationPath = ValidateDestination(args[TARGET_FOLDER_INDEX]);
+                SynchronizationInterval = ValidateInterval(args[SYNCHRONIZATION_INTERVAL_INDEX]);
+                LogFilePath = ValidateLogFilePath(args[LOG_FILE_PATH_INDEX]);
+            }
+        }
+        internal bool IsValid()
+        {
+            return !string.IsNullOrEmpty(SourcePath) &&
+                !string.IsNullOrEmpty(DestinationPath) &&
+                SynchronizationInterval > 0 &&
+                !string.IsNullOrEmpty(LogFilePath);
+        }
+        string ValidateSource(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Console.Error.WriteLine($"Error: Provided source directory folder path ({directoryPath}) does not exist.");
+                return PATH_ERROR;
+            }
+            return directoryPath;
+        }
+        string ValidateDestination(string directoryPath)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Console.WriteLine($"Info: Provided destination directory folder path ({directoryPath}) does not exist. Creating it now.");
+                Directory.CreateDirectory(directoryPath);
+            }
+            return directoryPath;
+        }
+        int ValidateInterval(string interval)
+        {
+            if (!int.TryParse(interval, out int result))
+            {
+                Console.Error.WriteLine($"Error: Provided synchronization interval ({interval}) is not a valid integer.");
+                return -1;
+            }
+            if (result <= 0)
+            {
+                Console.Error.WriteLine($"Error: Provided synchronization interval ({interval}) must be a positive integer.");
+                return -1;
+            }
+            return result * 1000; // Convert seconds to milliseconds
+        }
+
+        string ValidateLogFilePath(string logFilePath)
+        {
+            FileInfo logFileInfo = new FileInfo(logFilePath);
+            if (!logFileInfo.Directory.Exists)
+            {
+                Console.WriteLine($"Info: Directories in the provided log file path do not exist. Creating them now.");
+                logFileInfo.Directory.Create();
+            }
+            return logFilePath;
+        }
+    }
+}
---
+++ FileSynchronization/Program.cs
@@ -0,0 +1,18 @@
+﻿using FileSynchronization;
+
+class Program
+{
+
+    static int Main(string[] args)
+    {
+        CommandLineArguments commandLineArguments = new(args);
+        if (!commandLineArguments.IsValid())
+        {
+            Console.Error.WriteLine("Error: Provided arguments were not valid.\n Use the following format:\n \"FileSynchronization <sourcePath> <replicaPath> <synchronizationInterval> <logFilePath>\"");
+            return -1;
+        }
+        
+        Console.WriteLine("Arguments received from command line, starting synchronization process...");
+        return 0;
+    }
+}
